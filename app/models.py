9baa342fde2bc6440f401ba4649ab819d21f2686
Razi Ahmad <mail2razi4u@gmail.com>
2023-04-04 01:16:15 +0530

    patient and reminder tables created

--- app/models.py
+++ app/models.py
@@ -1,2 +1,21 @@
 from django.db import models
 
+from multiselectfield import MultiSelectField
+# Create your models here.
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=10,choices=(("Male","Male"),("Female","Female")))
+
+
+class Treatment(models.Model):
+    patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
+    disease = models.CharField(max_length=1000)
+    medicine = models.CharField(max_length=1000)
+    dosage = models.CharField(max_length=1000)
+    days = MultiSelectField(choices=(("Monday","Monday"),("Tuesday","Tuesday"),("Wednesday","Wednesday"),("Thursday","Thursday"),
+                                     ("Friday","Friday"),("Saturday","Saturday"),("Sunday","Sunday")
+                                     ))
+    
